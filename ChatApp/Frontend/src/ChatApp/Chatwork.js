df0c217c5dc5fe909e6205e9cae3bdecfccc473a
Kashif Hussain <kh9732494@gmail.com>
2023-09-20 00:11:44 +0500

    Chat App - Live
    Note:- Include dependencies before using this code

--- ChatApp/Frontend/src/ChatApp/Chatwork.js
+++ ChatApp/Frontend/src/ChatApp/Chatwork.js
@@ -1,4 +1,10 @@
 import React, { useEffect, useState, useRef } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+<script
+  src="https://kit.fontawesome.com/e3f9e15029.js"
+  crossorigin="anonymous"
+></script>;
 
 function Chat({ socket, username, room }) {
   const [currentMessage, setCurrentMessage] = useState("");
@@ -6,15 +12,17 @@ function Chat({ socket, username, room }) {
   const chatContainerRef = useRef(null);
 
   const sendMessage = async () => {
-    if (currentMessage !== "") {
+    if (currentMessage !== "" && currentMessage !== " ") {
+      const now = new Date();
       const messageData = {
         room: room,
         author: username,
         message: currentMessage,
-        time:
-          new Date(Date.now()).getHours() +
-          ":" +
-          new Date(Date.now()).getMinutes(),
+        time: now.toLocaleTimeString([], {
+          hour: "2-digit",
+          minute: "2-digit",
+          hour12: true,
+        }),
       };
 
       await socket.emit("send_message", messageData);
@@ -67,7 +75,7 @@ function Chat({ socket, username, room }) {
         <input
           type="text"
           value={currentMessage}
-          placeholder="Hello..."
+          placeholder="Enter your message here..."
           onChange={(event) => {
             setCurrentMessage(event.target.value);
           }}
@@ -75,172 +83,14 @@ function Chat({ socket, username, room }) {
             event.key === "Enter" && sendMessage();
           }}
         />
-        <button onClick={sendMessage}>&#8883;</button>
+        <button onClick={sendMessage}>
+          {/* <i class="fa-solid fa-paper-plane"></i> */}
+          {/* <FontAwesomeIcon icon="fa-solid fa-paper-plane" /> */}
+          <FontAwesomeIcon icon={faPaperPlane} />
+        </button>
       </div>
     </div>
   );
 }
 
 export default Chat;
-
-// import React, { useEffect, useState, useRef } from "react";
-
-// function Chat({ socket, username, room }) {
-//   const [currentMessage, setCurrentMessage] = useState("");
-//   const [messageList, setMessageList] = useState([]);
-//   const chatContainerRef = useRef(null);
-
-//   const sendMessage = async () => {
-//     if (currentMessage !== "") {
-//       const messageData = {
-//         room: room,
-//         author: username,
-//         message: currentMessage,
-//         time:
-//           new Date(Date.now()).getHours() +
-//           ":" +
-//           new Date(Date.now()).getMinutes(),
-//       };
-
-//       await socket.emit("send_message", messageData);
-//       setMessageList((list) => [...list, messageData]);
-//       setCurrentMessage("");
-//     }
-//   };
-
-//   useEffect(() => {
-//     socket.on("receive_message", (data) => {
-//       setMessageList((list) => [...list, data]);
-//       // Scroll to the bottom of the chat container when a new message is received
-//       if (chatContainerRef.current) {
-//         chatContainerRef.current.scrollIntoView({ behavior: "smooth" });
-//       }
-//     });
-//   }, [socket]);
-
-//   return (
-//     <div className="chat-window">
-//       <div className="chat-header">
-//         <p>Live Chat</p>
-//       </div>
-//       <div className="chat-body">
-//         <div className="message-container" ref={chatContainerRef}>
-//           {messageList.map((messageContent, index) => (
-//             <div
-//               className="message"
-//               id={username === messageContent.author ? "you" : "other"}
-//               key={index}
-//             >
-//               <div>
-//                 <div className="message-content">
-//                   <p>{messageContent.message}</p>
-//                 </div>
-//                 <div className="message-meta">
-//                   <p id="time">{messageContent.time}</p>
-//                   <p id="author">{messageContent.author}</p>
-//                 </div>
-//               </div>
-//             </div>
-//           ))}
-//         </div>
-//       </div>
-//       <div className="chat-footer">
-//         <input
-//           type="text"
-//           value={currentMessage}
-//           placeholder="Hello..."
-//           onChange={(event) => {
-//             setCurrentMessage(event.target.value);
-//           }}
-//           onKeyPress={(event) => {
-//             event.key === "Enter" && sendMessage();
-//           }}
-//         />
-//         <button onClick={sendMessage}>&#8883;</button>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Chat;
-
-
-
-// import React, { useEffect, useState } from "react";
-
-// import { scrollToBottom } from "react-scroll/modules/mixins/animate-scroll";
-
-// function Chat({ socket, username, room }) {
-//   const [currentMessage, setCurrentMessage] = useState("");
-//   const [messageList, setMessageList] = useState([]);
-
-//   const sendMessage = async () => {
-//     if (currentMessage !== "") {
-//       const messageData = {
-//         room: room,
-//         author: username,
-//         message: currentMessage,
-//         time:
-//           new Date(Date.now()).getHours() +
-//           ":" +
-//           new Date(Date.now()).getMinutes(),
-//       };
-
-//       await socket.emit("send_message", messageData);
-//       setMessageList((list) => [...list, messageData]);
-//       setCurrentMessage("");
-//     }
-//   };
-
-//   useEffect(() => {
-//     socket.on("receive_message", (data) => {
-//       setMessageList((list) => [...list, data]);
-//     });
-//   }, [socket]);
-
-//   return (
-//     <div className="chat-window">
-//       <div className="chat-header">
-//         <p>Live Chat</p>
-//       </div>
-//       <div className="chat-body">
-//         <scrollToBottom className="message-container">
-//           {messageList.map((messageContent) => {
-//             return (
-//               <div
-//                 className="message"
-//                 id={username === messageContent.author ? "you" : "other"}
-//               >
-//                 <div>
-//                   <div className="message-content">
-//                     <p>{messageContent.message}</p>
-//                   </div>
-//                   <div className="message-meta">
-//                     <p id="time">{messageContent.time}</p>
-//                     <p id="author">{messageContent.author}</p>
-//                   </div>
-//                 </div>
-//               </div>
-//             );
-//           })}
-//         </scrollToBottom>
-//       </div>
-//       <div className="chat-footer">
-//         <input
-//           type="text"
-//           value={currentMessage}
-//           placeholder="Hello..."
-//           onChange={(event) => {
-//             setCurrentMessage(event.target.value);
-//           }}
-//           onKeyPress={(event) => {
-//             event.key === "Enter" && sendMessage();
-//           }}
-//         />
-//         <button onClick={sendMessage}>&#8883;</button>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Chat;
--- ChatApp/Frontend/src/index.js
+++ ChatApp/Frontend/src/index.js
@@ -4,17 +4,76 @@ import "./style.css";
 
 import Chat from "./ChatApp/Chatwork.js";
 import { io } from "socket.io-client";
+
 const socket = io.connect("http://localhost:3001");
 
 function MyMultiPost() {
   const [username, setUsername] = useState("");
-  const [room, setRoom] = useState("");
+  const [room, setRoom] = useState(" ");
   const [showChat, setShowChat] = useState(false);
+  const [usernameError, setUsernameError] = useState("");
+  const [roomError, setRoomError] = useState("");
+  // const [showAlert, setShowAlert] = useState(false);
 
   const joinRoom = () => {
-    if (username !== "" && room !== "") {
+    let errorMessage = "";
+
+    // Regular expression to validate the username format.
+    const regex = /^[A-Z][a-z]{2,}(?:\s[A-Z][a-z]*)*$/;
+
+    // Check if the username matches the defined regular expression.
+    if (!regex.test(username)) {
+      // Check if the first character of the username is not UPPERCASE.
+      if (!/^[A-Z]/.test(username)) {
+        errorMessage = "First Character of Username Should be UPPERCASE";
+      }
+      // Check if the username does not contain at least two lowercase characters following the uppercase first character.
+      else if (!/^[a-z]{2,}/.test(username)) {
+        errorMessage =
+          "Username should contain atleast two lowercase alphabets after first UPPERCASE";
+      }
+      // Check if the last character of the username is not an alphabet character.
+      else if (!/\s[A-Z][a-z]*/.test(username)) {
+        errorMessage = "Last character of Username should be an alphabet";
+      }
+    }
+
+    // Check if the username contains one or more non-alphabet characters.
+    if (/[^\sa-zA-Z]+/.test(username)) {
+      errorMessage = "Username cannot contain numbers or special characters.";
+    }
+
+    // Check if the username is entirely in uppercase.
+    if (/^[A-Z ]+$/.test(username)) {
+      errorMessage = "Username must be Capitalized.";
+    }
+
+    // Check if username is less than or equal to 20 characters long
+    if (username.length > 25) {
+      errorMessage = "Username cannot be more than 25 characters long";
+    }
+
+    // Check if room ID is a number between 1 and 999
+    if (isNaN(room) || room < 1 || room > 999) {
+      errorMessage = "Room ID must be a number between 1 and 999";
+    }
+
+    if (errorMessage === "") {
       socket.emit("join_room", room);
       setShowChat(true);
+    } else {
+      if (usernameError !== "") {
+        setUsernameError("");
+      }
+      if (roomError !== "") {
+        setRoomError("");
+      }
+      if (errorMessage.includes("Username")) {
+        setUsernameError(errorMessage);
+      } else {
+        setRoomError(errorMessage);
+      }
+      // setShowAlert(true);
     }
   };
 
@@ -22,36 +81,57 @@ function MyMultiPost() {
     <div className="App">
       {!showChat ? (
         <div className="joinChatContainer">
-          <h3>Join A Chat</h3>
-          
+          <h3>Join a chat</h3>
+
           <input
             type="text"
+            minLength={3}
+            maxLength={25}
+            required
             placeholder="Kashif Hussain"
+            value={username}
             onChange={(event) => {
               setUsername(event.target.value);
+              setUsernameError("");
             }}
           />
+          <div className="error">{usernameError}</div>
+
           <input
-            type="text"
+            type="number"
+            min={1}
+            max={999}
+            required
+            inputMode="numeric"
+            pattern="[0-9]*"
             placeholder="Room ID..."
+            value={room}
             onChange={(event) => {
               setRoom(event.target.value);
+              setRoomError("");
             }}
           />
-          <button onClick={joinRoom}>Join A Room</button>
+          <div className="error">{roomError}</div>
+
+          <button onClick={joinRoom}>Join chat room</button>
         </div>
       ) : (
-        <Chat socket={socket} username={username} room={room} />
+        <Chat
+          socket={socket}
+          username={username}
+          room={room}
+          // setShowAlert={setShowAlert}
+        />
       )}
+      {/* {showAlert && (
+        <div className="alert alert-danger my-auto" role="alert">
+          Error joining Room
+        </div>
+      )} */}
     </div>
   );
 }
+
 var root = ReactDOM.createRoot(document.getElementById("root"));
 
-let value1 = (
-  <>
-    <MyMultiPost />
-  </>
-);
-
-root.render(value1);
+root.render(<MyMultiPost />);
